2783c9c87499db8357553af5ddf18508cede0950
akraradets <akraradets@gmail.com>
2026-04-29 08:51:35 +0700

    feat: update celery template

--- src/project/celery.py
+++ src/project/celery.py
@@ -1,3 +1,4 @@
+import logging
 from celery import Celery # type: ignore
 from celery.result import AsyncResult # type: ignore
 from typing import Annotated
@@ -8,7 +9,9 @@ from rich.pretty import pprint
 import typer
 from typer import Typer
 
+logger = logging.getLogger(__name__)
 REDIS_URL = os.environ["REDIS_URL"]
+logger.debug(f"REDIS_URL={REDIS_URL}"
 
 queue = Celery(__name__, broker=REDIS_URL, backend=REDIS_URL, task_track_started=True)
 console = Console(highlight=False)
@@ -56,3 +59,22 @@ def clear(job_id:Annotated[str, typer.Argument(help="The `job_id` of celery job.
     job.forget()
     sleep(1) # sleep here and the job status has time to process
     console.print(f"[green]job_id={job_id} is cleared.")
+
+@queue.task # type: ignore
+def _sample_job() -> str:
+    logger.debug("a")
+    for i in range(10):
+        logger.info(f"iteration {i}")
+        sleep(1)
+    logger.debug("a")
+    return "result"
+
+@cli.command()
+def sample_job():
+    """
+    This will submit a job to celery
+    """
+    logging.debug(f"Calling `sample_job` from celery.py")
+    job_id:str = _sample_job.delay() # type: ignore
+    logging.info(f"celery job_id={job_id}")
+    print(f"{job_id}")
